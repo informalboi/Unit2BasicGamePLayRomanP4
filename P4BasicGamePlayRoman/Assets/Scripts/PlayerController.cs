d32bb1f806ad933cafa2cde6d971034a4fb82317
informalboi <733416@student.dusd.net>
2026-03-05 11:45:58 -0800

    lebrion

---
+++ P4BasicGamePlayRoman/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
+    // Update is called once per frame
+    void Update()
+    {
+        if ( transform.position.x <-xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
