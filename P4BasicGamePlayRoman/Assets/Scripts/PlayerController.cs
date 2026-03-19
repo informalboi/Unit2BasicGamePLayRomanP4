e7630f9de10465bc3974e0a20b5f06c9c3369304
informalboi <733416@student.dusd.net>
2026-03-19 10:16:07 -0700

    lebron

--- P4BasicGamePlayRoman/Assets/Scripts/PlayerController.cs
+++ P4BasicGamePlayRoman/Assets/Scripts/PlayerController.cs
@@ -2,25 +2,24 @@
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    public float horizontalInput;
+    public float speed = 10.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
-    public float horizontalInput;
-    public float speed = 10.0f;
-    public float xRange = 10;
+
+    
+    
     // Update is called once per frame
     void Update()
     {
-        // Keep the player in bonds
-        if ( transform.position.x <-xRange)
+        if( transform.position.x < -10)
         {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
         }
-        if (transform.position.x > xRange)
-        {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
