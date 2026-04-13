a8d241985fd29f2e3ae97fdf5c7650d8f30edda7
informalboi <733416@student.dusd.net>
2026-04-12 23:46:54 -0700

    finish

--- P4BasicGamePlayRoman/Assets/Scripts/PlayerController.cs
+++ P4BasicGamePlayRoman/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,10 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public float xRange = 10.0f;
 
     public GameObject projectilePrefab;
+
+    public float zMin;
+    public float zMax;
+    public float verticalInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,8 +22,10 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      
+
         // Keep the player in bounds
-        if( transform.position.x < -xRange)
+        if ( transform.position.x < -xRange)
         {
             transform.position = new Vector3(-10, transform.position.y, transform.position.z);
         }
@@ -27,7 +33,27 @@ public class NewMonoBehaviourScript : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        if (transform.position.z < zMin)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y,
+            zMin);
+        }
+        if (transform.position.z > zMax)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y,
+            zMax);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
     }
 }
